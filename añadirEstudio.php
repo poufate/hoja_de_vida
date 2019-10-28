0267a83df5f66c2796dcf7d56dd41a396c586c77
Poufate <poutecermi@gmail.com>
2019-10-27 18:56:59 -0600

    maquetado, pocos cambios restantes pero listo para implementar formularios y probar conexiones

---
+++ añadirEstudio.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>pagina para añadir estudio o servicio para hacerlo</h1>
+<p>beep boop, esta pagina es temporal</p>
+</body>
+</html>
